adab2ec399ba4bf7ea156d75f78dae3286c93e04
DiegoGarcia73 <diegogarcia9123@gmail.com>
2024-10-10 21:08:43 -0600

    Se comparten ejercicios realizados
    Ejercicios de JQuery, Web Service ASMX

---
+++ 3.-Web Forms/HolaMundoWSASMX/HolaMundoWSASMX/WSAlumnos.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Entities;
+using BusinessLogic;
+
+namespace HolaMundoWSASMX
+{
+    /// <summary>
+    /// Descripción breve de WSAlumnos
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")] //Decoradores //Adicionar elementos
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)] //
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    [System.Web.Script.Services.ScriptService]
+    public class WSAlumnos : System.Web.Services.WebService
+    {
+
+        [WebMethod] //Decorador. Permite exponer los métodos, si no la tiene no serán expuestos los métodos. Depende de los métodos que se agreguen se añade esta clase decorador
+        public ItemTablaISR CalcularISR(int id)
+        {
+           NAlumno oNAlumno = new NAlumno();
+           ItemTablaISR oISR = oNAlumno.CalcularISR(id);
+           return oISR;
+        }
+    }
+}
---
+++ 3.-Web Forms/WebServiceASMX/WebServiceASMX/WSAlumnos.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Entities;
+using BusinessLogic;
+
+namespace WebServiceASMX
+{
+    /// <summary>
+    /// Descripción breve de WSAlumnos
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    [System.Web.Script.Services.ScriptService]
+    public class WSAlumnos : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public ItemTablaISR CalcularISR(int id)
+        {
+            NAlumno oNAlumnoISR = new NAlumno();
+            ItemTablaISR oISR = oNAlumnoISR.CalcularISR(id);
+            return oISR;
+        }
+        [WebMethod]
+        public AportacionesIMSS CalcularIMSS(int id)
+        {
+            //Invocación del método para calcular las Aportaciones IMSS a través de la
+            //creación de un objeto tipo Aportaciones IMSS llamado oIMSS
+            NAlumno NAlumnoIMSS = new NAlumno();
+            AportacionesIMSS oIMSS = NAlumnoIMSS.CalcularIMSS(id);
+            return oIMSS;
+        }
+    }
+}
